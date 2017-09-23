3bf072dda0ab83cfc5f5b80fe6e3d7437e58c313
firecrafty <bluer2288@students.pike.k12.in.us>
2017-09-23 11:56:56 -0400

    PROS Project Template

---
+++ src/auto.c
@@ -0,0 +1,43 @@
+/**
+ * @file auto.c
+ * @brief File for autonomous code
+ *
+ * This file should contain the user autonomous() function and any functions
+ * related to it.
+ *
+ * Copyright (C) 2017-2018  Pike RoboDevils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "main.h"
+
+/*
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped by the kernel. Re-enabling the robot will restart the task,
+ * not re-start it from where it left off.
+ *
+ * Code running in the autonomous task cannot access information from the VEX
+ * Joystick. However, the autonomous function can be invoked from another task
+ * if a VEX Competition Switch is not available, and it can access joystick
+ * information if called in this way.
+ *
+ * The autonomous task may exit, unlike operatorControl() which should never
+ * exit. If it does so, the robot will await a switch to another mode or
+ * disable/enable cycle.
+ */
+void autonomous() {}
